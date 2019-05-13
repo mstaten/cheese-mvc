6544c908e8a03f27e745d99502f102f356106213
Michelle Staten <mnstaten@yahoo.com>
2019-05-13 15:13:54 -0500

    Studio 4 done: Remove Cheese and validate Add Cheese input

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -32,17 +32,58 @@ public class CheeseController {
         return "cheese/add";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
+    public String validateAddForm(@RequestParam String cheeseName) {
+        if (cheeseName.equals("")) {
+            return "Must enter cheese name";
+        }
 
+        // check that each char is alphabetic
+        for (Character c : cheeseName.toCharArray()) {
+            if (!Character.isAlphabetic(c) && !Character.isWhitespace(c)) {
+                return "Only alphabetic characters and spaces allowed";
+            }
+        }
+        return "";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(Model model, @RequestParam String cheeseName, @RequestParam String cheeseDesc) {
+
+        String nameError = validateAddForm(cheeseName);
+
+        if (!nameError.equals("")) {
+            model.addAttribute("cheeses", cheeses);
+            model.addAttribute("title", "Add Cheeses");
+            model.addAttribute("cheeseDesc", cheeseDesc);
+            model.addAttribute("nameError", nameError);
+            return "cheese/add";
+        }
         cheeses.put(cheeseName, cheeseDesc);
 
         // Redirect to /cheese
         return "redirect:";
     }
 
-    @RequestMapping(value = "")
-    public static String watchIt() {
-        return "redirect:/cheese";
+    // Request path: cheese/remove
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
     }
+
+
+    // Request path: cheese/remove
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheeseList) {
+
+        for (String aCheese : cheeseList ) {
+            cheeses.remove(aCheese);
+        }
+
+        // Redirect to cheese/
+        return "redirect:";
+    }
+
 }
